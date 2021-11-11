88215f1d8eee2b9c27985bd8083a7fe570aaefde
Vincent Hardouin <vincenthardouin@icloud.com>
2021-11-11 18:56:03 +0100

    Initial commit with eslint and prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  extends: ['plugin:prettier/recommended'],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
+};
