d265bd1f7d211c08b9eabb3f9d66b0bc577d6513
VincentHardouin <VincentHardouin@users.noreply.github.com>
2022-03-21 09:16:40 +0000

    deploy: b373596a483a23d810462d8410c6a5d3e7f8f708

--- _nuxt/static/1647767794/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647767794",layout:"default",error:a,serverRendered:true,routePath:b,config:{apiUrl:"https:\u002F\u002Fapi-wods.vincenthardouin.dev",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
