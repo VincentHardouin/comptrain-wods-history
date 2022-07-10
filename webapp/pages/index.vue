9d6b12ed9143e061fedbc90e3f8ca24f5b8a4b79
Vincent Hardouin <vincenthardouin@icloud.com>
2022-07-10 23:46:14 +0200

    sr: remove style

--- webapp/pages/index.vue
+++ webapp/pages/index.vue
@@ -18,36 +18,3 @@ export default {
   },
 };
 </script>
-
-<style>
-body {
-  margin: 0;
-  padding: 0;
-}
-
-.index {
-  padding: 15px;
-  background: #556270;
-  background: linear-gradient(45deg, #f1f1f1ff, #fbd8d8ff);
-}
-
-.workouts {
-  display: flex;
-  flex-direction: column;
-  gap: 21px;
-}
-
-.workout {
-  background-color: #ffff;
-  padding: 28px 21px;
-  width: 250px;
-  display: flex;
-  flex-direction: column;
-  overflow-wrap: break-word;
-  border-radius: 14px;
-}
-
-.workout h2 {
-  margin-top: 0;
-}
-</style>
